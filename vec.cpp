ed48da91e433206fa44e0268c72ad42a1b0ab933
NAVID SHAMSZADEH <nshamszadeh@umail.ucsb.edu>
2017-10-17 16:02:15 -0700

    Update vec.cpp

--- vec.cpp
+++ vec.cpp
@@ -40,7 +40,7 @@ Vec3 operator +(const Vec3& v1, const Vec3& v2)
  return v3;
 }
 
-Vec3 operator *(const Vec3& v1, double d)
+Vec3 operator *(const Vec3& v1, const double& d)
 {
    Vec3 v2 = Vec3(d*v1.x, d*v1.y, d*v1.z);
    return v2;
